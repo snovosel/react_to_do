143a8bdd28dc317b3f0d609ba179cce614a7779f
Steve Novosel <novosel991@gmail.com>
2018-01-20 13:15:37 -0500

    all files included

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-Hello all
+React Starter App
+-------------------
 
+
+This is a starter template for React apps with a simple
+setup so that time is spent more on coding rather than on config.
+
+
+To set up locally on your machine:
---
+++ app/index.js
@@ -0,0 +1,13 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {App} from './components/App.js';
+
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
